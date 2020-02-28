056de13f620bb715704db0922b2cce53e1a964d2
arthur-sanches <arthur.sanches96@gmail.com>
2020-02-27 21:24:24 -0300

    The blockchain code was turned into an API using Flask and a website interface was implemented.

--- blockchain.py
+++ blockchain.py
@@ -108,6 +108,8 @@ class Blockchain:
 
     def get_balance(self):
         """Calculate and return the balance for a participant."""
+        if self.hosting_node == None:
+            return None
         participant = self.hosting_node
         # Fetch a list of all sent coin amounts for the given person (empty lists are returned if the person was NOT the sender)
         # This fetches sent amounts of transactions that were already included in blocks of the blockchain
@@ -159,7 +161,7 @@ class Blockchain:
     def mine_block(self):
         """Create a new block and add open transactions to it."""
         if self.hosting_node == None:
-            return False
+            return None
         # Fetch the currently last block of the blockchain
         last_block = self.__chain[-1]
         # Hash the last block (=> to be able to compare it to the stored hash value)
@@ -178,7 +180,7 @@ class Blockchain:
         copied_transactions = self.__open_transactions[:]
         for tx in copied_transactions:
             if not Wallet.verify_transaction(tx):
-                return False
+                return None
         copied_transactions.append(reward_transaction)
         block = Block(len(self.__chain), hashed_block,
                       copied_transactions, proof)
@@ -186,4 +188,4 @@ class Blockchain:
         self.__chain.append(block)
         self.__open_transactions = []
         self.save_data()
-        return True
+        return block
